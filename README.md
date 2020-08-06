d74240022d290d96a6d4c989325e655e4939a4ae
Shafeeq Kunnakkadan <shafeeq.k.aux@go-jek.com>
2020-08-06 13:45:22 +0530

    Add usage example to README
    Co-authored-by: Nipun Jindal <nipun.jindal@go-jek.com>

--- README.md
+++ README.md
@@ -8,4 +8,47 @@ An asynchronous job worker library for Clojure
 
 ## Examples
 
+Hello world example:
+
+``` clojure
+(ns your.ns
+  (:require [async-worker.core :as async-worker]))
+
+(defn fn-1 [args]
+  (prn {:my-fn args :at (System/currentTimeMillis)})
+  (throw (Exception. "Retry")))
+
+(defn fn-2 [payload]
+  (prn {:my-fn-2 payload :retry :no})
+  (throw (Exception. "Fail and move to dead set")))
+
+(def executor (java.util.concurrent.Executors/newFixedThreadPool 5))
+
+(def config {:namespace "trips"
+             :rabbitmq  {:hosts              ["localhost"]
+                         :port               5672
+                         :username           "guest"
+                         :password           "guest"
+                         :admin-port         15672
+                         :connection-timeout 2000
+                         :subscriber-count   5}
+             :executor  executor
+             :jobs      {:job-1 {:retry-max        5
+                                 :retry-timeout-ms 1000
+                                 :handler-fn       fn-1}
+                         :job-2 {:handler-fn fn-2}}})
+
+(defonce worker (atom nil))
+
+;; start and store the worker config/connection
+(reset! worker (async-worker/start config))
+
+(async-worker/enqueue @worker :job-1 {:hello :world-1})
+(async-worker/enqueue @worker :job-2 {:hello :world-2})
+
+;; stop the worker
+(async-worker/stop @worker)
+```
+
+
 ## Contributing
--- src/async_worker/core.clj
+++ src/async_worker/core.clj
@@ -1,36 +1,25 @@
 (ns async-worker.core
-  (:require [async-worker.rabbitmq.connection :as conn]
-            [async-worker.rabbitmq.queue :as queue]
-            [async-worker.rabbitmq.producer :as producer]
+  (:require [async-worker.handler :as handler]
+            [async-worker.rabbitmq.cluster :as cluster]
+            [async-worker.rabbitmq.connection :as conn]
             [async-worker.rabbitmq.consumer :as consumer]
-            [async-worker.rabbitmq.dead-set :as dead-set]
             [async-worker.rabbitmq.exchange :as exchange]
-            [async-worker.handler :as handler]
-            [async-worker.rabbitmq.cluster :as cluster])
-  (:import [java.util.concurrent Executors]))
+            [async-worker.rabbitmq.producer :as producer]
+            [async-worker.rabbitmq.queue :as queue]))
 
-(defonce rmq-conn (atom nil))
-
-(defn new-conn [{:keys [hosts port username password connection-timeout executor] :as config}]
-  (conn/start-connection {:hosts                 hosts
-                          :port                  port
-                          :username              username
-                          :password              password
-                          :connection-timeout    connection-timeout
-                          :executor              executor
-                          :automatically-recover true}))
-
-(defn init [{:keys [namespace rabbitmq executor jobs] :as config}]
-  (let [rabbitmq-conn (new-conn (assoc rabbitmq :executor executor))
+(defn start [{:keys [namespace rabbitmq executor jobs] :as config}]
+  (let [rabbitmq-conn (conn/start-connection
+                       (merge rabbitmq {:executor executor :automatically-recover true}))
         exchange-name (exchange/name namespace)]
     (exchange/declare rabbitmq-conn exchange-name)
     (queue/init rabbitmq-conn namespace exchange-name)
     (doseq [[job-name job-config] jobs]
-      (queue/setup-delay-queues rabbitmq-conn
-                                namespace
-                                exchange-name
-                                (:retry-max job-config)
-                                (:retry-timeout-ms job-config)))
+      (when (:retry-max job-config)
+        (queue/setup-delay-queues rabbitmq-conn
+                                  namespace
+                                  exchange-name
+                                  (:retry-max job-config)
+                                  (:retry-timeout-ms job-config))))
     (consumer/start-subscribers rabbitmq-conn
                                 namespace
                                 (:subscriber-count rabbitmq)
@@ -38,6 +27,8 @@
     (cluster/set-ha-policy rabbitmq namespace)
     (assoc config :connection rabbitmq-conn)))
 
+(defn stop [worker]
+  (conn/stop-connection (:connection worker)))
 
 (defn enqueue [config job-name args]
   (let [job-config (get-in config [:jobs job-name])]
@@ -48,33 +39,3 @@
                        :current-iteration 0
                        :retry-max         (:retry-max job-config)
                        :retry-timeout-ms  (:retry-timeout-ms job-config)})))
-
-(comment
-  (defn my-fn [payload]
-    (prn {:my-fn payload :at (System/currentTimeMillis)})
-    (throw (Exception. "Retry")))
-  (defn my-fn-2 [payload]
-    (prn {:my-fn-2 payload}))
-
-  (def config {:namespace "trips"
-               :rabbitmq  {:hosts              ["localhost"]
-                           :port               5672
-                           :username           "guest"
-                           :password           "guest"
-                           :admin-port         15672
-                           :connection-timeout 2000
-                           :subscriber-count   5}
-               :jobs        {:my-job   {:retry-max        5
-                                        :retry-timeout-ms 1000
-                                        :handler-fn       my-fn}
-                             :my-job-2 {:retry-max        5
-                                        :retry-timeout-ms 2
-                                        :handler-fn       my-fn-2}}})
-
-
-
-  (defonce worker (atom nil))
-  (reset! worker (init config))
-  #_(conn/stop-connection (:connection @worker))
-
-  (enqueue @worker :my-job {:hello :world}))
