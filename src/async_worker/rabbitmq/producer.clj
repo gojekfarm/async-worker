03166df0c371dadbf001a0ccb425d10710d83d5f
Shafeeq Kunnakkadan <shafeeq.k.aux@go-jek.com>
2020-08-06 17:15:13 +0530

    Make publishing messages reliable
    - If the publish fails during the requeue in handler, it can tirgger
      re-processing of message until requeue succeeds
    
    Co-authored-by: Nipun Jindal <nipun.jindal@go-jek.com>

--- src/async_worker/rabbitmq/producer.clj
+++ src/async_worker/rabbitmq/producer.clj
@@ -2,37 +2,53 @@
   (:require [clojure.tools.logging :as log]
             [langohr.basic :as lb]
             [langohr.channel :as lch]
+            [langohr.confirm :as lconf]
             [taoensso.nippy :as nippy]
             [async-worker.rabbitmq.queue :as queue]
             [async-worker.rabbitmq.exchange :as exchange]
             [async-worker.utils :as u]))
 
+(def confirmation-timeout-ms 1000)
+
 (defn- properties-for-publish
   [expiration]
   (let [props {:content-type "application/octet-stream"
                :persistent   true
+               :mandatory    true
                :headers      {}}]
     (if expiration
       (assoc props :expiration (str expiration))
       props)))
 
+(defn- die-if-message-returned [p]
+  (when (realized? p)
+    (throw (ex-info "Message returned during publish" @p))))
+
 (defn- publish
+  "Tries to publish the message reliably. Retries 5 times with 100ms wait; throws exception if unsuccessful
+
+  Unrouted messges are returned with ack and are handled by the return-handler.
+
+  Nack-ed messages are handled by wait-for-confirms-or-die.
+  Throws exception if the messages are not acked within `confirmation-timeout-ms`"
   ([connection exchange routing-key message-payload]
    (publish connection exchange routing-key message-payload nil))
   ([connection exchange routing-key message-payload expiration]
-   (try
-     (u/with-retry {:count 5 :wait 100}
+   (u/with-retry {:count 5 :wait 100}
+     (let [return-promise (promise)
+           return-handler (lb/return-listener (fn [reply-code reply-text exchange routing-key properties body]
+                                                (deliver return-promise {:reply-code reply-code
+                                                                         :reply-text reply-text})))]
        (with-open [ch (lch/open connection)]
+         (.addReturnListener ch return-handler)
+         (lconf/select ch)
          (lb/publish ch
                      exchange
                      routing-key
                      (nippy/freeze message-payload)
-                     (properties-for-publish expiration))))
-     (catch Throwable e
-       (log/error "Publishing message to rabbitmq failed with error " (.getMessage e))
-       (throw e)
-       #_(sentry/report-error sentry-reporter e
-                            "Pushing message to rabbitmq failed, data: " message-payload)))))
+                     (properties-for-publish expiration))
+         (lconf/wait-for-confirms-or-die ch confirmation-timeout-ms)
+         (die-if-message-returned return-promise))))))
 
 (defn enqueue [connection namespace message]
   (publish connection
